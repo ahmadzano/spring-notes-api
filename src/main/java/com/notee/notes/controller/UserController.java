b106405f9c72603b221619edc537cd211c7a483d
ahmaddo <a.ahmad@outlook.de>
2019-03-14 23:02:45 +0100

    use response entity as rest controller data type

--- src/main/java/com/notee/notes/controller/UserController.java
+++ src/main/java/com/notee/notes/controller/UserController.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import com.notee.notes.model.User;
 import com.notee.notes.repository.UserRepository;
@@ -15,15 +13,20 @@ import com.notee.notes.repository.UserRepository;
 @RestController
 public class UserController {
 
-    @Autowired
+    final private
     UserRepository userRepository;
 
-    @RequestMapping(path = "/users", method = RequestMethod.GET)
-    public Iterable<User> getUser(@RequestParam(name = "id", required = false) Long id) {
-        ArrayList<User> users = new ArrayList<User>();
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        if (id != null) {
-            Optional<User> user = userRepository.findById(id);
+    @RequestMapping(value = {"/users", "/users/{userId}"}, method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable(value = "userId", required = false) Long userId) {
+        ArrayList<User> users = new ArrayList<>();
+
+        if (userId != null) {
+            Optional<User> user = userRepository.findById(userId);
             if (user.isPresent()) {
                 users.add(user.get());
             }
@@ -31,14 +34,24 @@ public class UserController {
             users = (ArrayList<User>) userRepository.findAll();
         }
 
-        return users;
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/users/{userId}/notes", method = RequestMethod.GET)
+    public ResponseEntity<?> getUserNotes(@PathVariable Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new String[]{"user not found"}, HttpStatus.NOT_FOUND);
     }
 
     @RequestMapping(path = "/users", method = RequestMethod.POST)
-    public User createUser(@RequestParam(name = "username") String username,
-                                     @RequestParam(name = "email", required = false) String email) {
+    public ResponseEntity<?> createUser(@RequestParam(name = "username") String username,
+                                        @RequestParam(name = "email", required = false) String email) {
         User newUser = new User(username, email);
 
-        return userRepository.save(newUser);
+        return new ResponseEntity<>(userRepository.save(newUser), HttpStatus.CREATED);
     }
 }
