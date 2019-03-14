e8ee109850c957292be8455c5a016a43426d698b
ahmaddo <a.ahmad@outlook.de>
2019-03-14 19:59:53 +0100

    implement create note endpoint

--- src/main/java/com/notee/notes/controller/NoteController.java
+++ src/main/java/com/notee/notes/controller/NoteController.java
@@ -1,8 +1,12 @@
 package com.notee.notes.controller;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.UUID;
 
+import com.notee.notes.model.User;
+import com.notee.notes.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +21,8 @@ public class NoteController {
 
     @Autowired
     private NoteRepository noteRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @RequestMapping(path = "/notes", method = RequestMethod.GET)
     public Iterable<Note> getNote(@RequestParam(name = "id", required = false) Long id) {
@@ -31,5 +37,22 @@ public class NoteController {
         }
 
         return notes;
+    }
+
+    @RequestMapping(path = "notes", method = RequestMethod.POST)
+    public Note createNote(@RequestParam(name = "content", required = false) String content, @RequestParam(name = "username") String username) {
+        Note newNote = new Note();
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            return newNote;
+        }
+
+        newNote.setContent(content);
+        newNote.setUserId(user.getId());
+        UUID uuid = UUID.randomUUID();
+        newNote.setUuid(uuid);
+        newNote.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+
+        return noteRepository.save(newNote);
     }
 }
