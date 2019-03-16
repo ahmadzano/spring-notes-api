31b2afa50d1bb65ab6415437dfe0c0590136db59
ahmaddo <a.ahmad@outlook.de>
2019-03-16 22:51:24 +0100

    change notes endpoint implementation

--- src/main/java/com/notee/notes/controller/NoteController.java
+++ src/main/java/com/notee/notes/controller/NoteController.java
@@ -1,17 +1,15 @@
 package com.notee.notes.controller;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
 import com.notee.notes.model.User;
 import com.notee.notes.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.notee.notes.model.Note;
 import com.notee.notes.repository.NoteRepository;
@@ -19,40 +17,41 @@ import com.notee.notes.repository.NoteRepository;
 @RestController
 public class NoteController {
 
-    @Autowired
-    private NoteRepository noteRepository;
-    @Autowired
-    private UserRepository userRepository;
+    private final NoteRepository noteRepository;
+    private final UserRepository userRepository;
 
-    @RequestMapping(path = "/notes", method = RequestMethod.GET)
-    public Iterable<Note> getNote(@RequestParam(name = "id", required = false) Long id) {
-        ArrayList<Note> notes = new ArrayList<Note>();
-        if (id != null) {
-            Optional<Note> note = noteRepository.findById(id);
-            if (note.isPresent()) {
-                notes.add(note.get());
-            }
-        } else {
-            notes = (ArrayList<Note>) noteRepository.findAll();
-        }
-
-        return notes;
+    @Autowired
+    public NoteController(NoteRepository noteRepository, UserRepository userRepository) {
+        this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
     }
 
-    @RequestMapping(path = "notes", method = RequestMethod.POST)
-    public Note createNote(@RequestParam(name = "content", required = false) String content, @RequestParam(name = "username") String username) {
+    @RequestMapping(value = "/notes/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getNote(@PathVariable("id") Long noteId) {
+        Optional<Note> note = noteRepository.findById(noteId);
+        if (note.isPresent()) {
+            return new ResponseEntity<>(note.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<Note>(HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(path = "/notes", method = RequestMethod.POST)
+    public ResponseEntity<?> createNote(@RequestParam(name = "content", required = false) String content,
+                                        @RequestParam(name = "username") String username,
+                                        @RequestParam(name = "type") String type) {
         Note newNote = new Note();
         User user = userRepository.findByUsername(username);
         if (user == null) {
-            return newNote;
+            return new ResponseEntity<Note>(HttpStatus.NOT_FOUND);
         }
 
         newNote.setContent(content);
         newNote.setUserId(user.getId());
-        UUID uuid = UUID.randomUUID();
-        newNote.setUuid(uuid);
+        newNote.setUuid(UUID.randomUUID());
+        newNote.setType(type);
         newNote.setCreatedAt(new Timestamp(System.currentTimeMillis()));
 
-        return noteRepository.save(newNote);
+        return new ResponseEntity<>(noteRepository.save(newNote), HttpStatus.OK);
     }
 }
