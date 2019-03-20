43024d87ddffbebd683c04d1dec525c10d85ca66
ahmaddo <a.ahmad@outlook.de>
2019-03-20 21:59:10 +0100

    remove primary datasource

--- src/main/java/com/notee/notes/config/PersistenceConfiguration.java
+++ src/main/java/com/notee/notes/config/PersistenceConfiguration.java
@@ -1,25 +1,12 @@
 package com.notee.notes.config;
 
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.context.annotation.*;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    @Profile("dev")
-    public DataSource primaryDataSource() {
-        return DataSourceBuilder.create()
-                .url("jdbc:h2:file:~/default")
-                .username("sa")
-                .password("")
-                .build();
-    }
-
     @Bean
     @Profile("prod")
     public DataSource secondaryDataSource() {
