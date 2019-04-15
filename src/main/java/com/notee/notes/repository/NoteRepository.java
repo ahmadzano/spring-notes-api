d6b1c45063bf4c48cc4806bfe19239d75f50e0f1
ahmaddo <a.ahmad@outlook.de>
2019-04-15 20:16:19 +0200

    implement find notes by user id

--- src/main/java/com/notee/notes/repository/NoteRepository.java
+++ src/main/java/com/notee/notes/repository/NoteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.notee.notes.model.Note;
 
 public interface NoteRepository extends CrudRepository<Note, Long> {
-
+    Iterable<Note> findByUserId(Long userId);
 }
